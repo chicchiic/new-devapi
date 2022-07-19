cac6cf181a5fdd12c8f0974518268eba48cb756d
inew <chicchiic@gmail.com>
2022-07-19 17:33:46 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template(
+        "hello_there.html",
+        now=datetime.now,
+    )
+
+@app.route("/apipy/<name>")
+def apipy(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    content = [{
+        "name": name,
+        "now_format" : formatted_now,
+        "now": now
+        }]
+    return jsonify(content)
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=80, host='0.0.0.0')
